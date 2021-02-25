3050c36b05b0cdd9d114a31a0b48ba4e87fd9e8a
= <=>
2021-02-25 12:58:41 -0800

    Initial commit

---
+++ ShanmukhaJava/src/test/java/TestNG/SampleTest.java
@@ -0,0 +1,19 @@
+package TestNG;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+		
+		@Test
+		public void PrintHello() {
+			System.out.println("Sample Test is Executed");
+		}
+		@Test
+		private void PrintNumbers() {
+			System.out.println("Printing the numbers");
+		}
+
+	
+
+}
